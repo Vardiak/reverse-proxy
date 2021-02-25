0d3d21456f5f462860a6fca45fbcef84a6842077
Vardiak <contact@vardiak.fr>
2021-02-25 19:22:48 +0100

    Map .zsh_history

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build . -t epita -f ./Dockerfile
-docker run --rm --privileged -v $(pwd):/root/data --workdir /root/data -it epita zsh
+docker run --rm --privileged -p 8000:8000 -v $(pwd):/root/data -v $(pwd)/.zsh_history:/root/.zsh_history --workdir /root/data -it epita zsh
